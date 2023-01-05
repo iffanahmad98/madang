ed30af97c9775bd47b044aa3d5104cfbe1a4864d
iffanahmad98 <iffanahmad98@gmail.com>
2023-01-05 20:21:12 +0700

    create home

---
+++ src/views/FoodsView.vue
@@ -0,0 +1,21 @@
+export <template>
+  <div>
+    <Navbar />
+    <h2>Daftar Makanan</h2>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+    name : "FoodsView",
+    components: {
+        Navbar,
+    }
+}
+</script>
+
+<style>
+
+</style>
