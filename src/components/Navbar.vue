c2a62c12cf44397e2ea87b5fdaa85c4682cfe7c5
iffanahmad98 <iffanahmad98@gmail.com>
2023-01-09 09:41:36 +0700

    update badge for icon jumlah pesanan

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -28,7 +28,7 @@ export <template>
           <li class="nav-item">
             <router-link class="nav-link" to="/keranjang"
               >Keranjang<b-icon-bag></b-icon-bag
-              ><span class="badge badge-success ml-2">0</span></router-link
+              ><span class="badge badge-success ml-2">{{ jumlah_pesanans.length }}</span></router-link
             >
           </li>
         </ul>
@@ -38,9 +38,26 @@ export <template>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
   name: "navBar",
+  data() {
+    return {
+      jumlah_pesanans: []
+    }
+  },
+  methods: {
+    setJumlah(data) {
+      this.jumlah_pesanans = data
+    }
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/keranjangs")
+      .then((response) => this.setJumlah(response.data))
+      .catch((error) => console.log("GAGAL", error));
+  },
 };
 </script>
 
--- src/views/Keranjang.vue
+++ src/views/Keranjang.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+
 import Navbar from "@/components/Navbar.vue";
 
 export default {
